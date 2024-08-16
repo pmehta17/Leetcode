3a1b90965362914fd27c3f50347675885e1eaca8
Pratham Mehta <81605115+pmehta17@users.noreply.github.com>
2024-08-15 22:06:23 -0400

    Time: 37 ms (40.39%) | Memory: 16.5 MB (34.66%) - LeetSync

---
+++ 290-word-pattern/word-pattern.py
@@ -0,0 +1,34 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+
+        # Questions: 
+        # 1. Can we assume the number of charcters in the pattern will be the same as the number of words in the string
+        # 2. Will we only have characters and words? Should it be case senstive? Special characters?
+
+        wordmap = {}
+        str_set = set()
+
+
+        s = list(s.split(" "))
+
+        if len(pattern) != len(s): 
+            return False
+
+        for i in range(len(pattern)):
+            if pattern[i] in wordmap: 
+                if wordmap[pattern[i]] != s[i]:
+                    return False
+                    
+            else: 
+                if s[i] in str_set:
+                    return False
+                    
+                wordmap[pattern[i]] = s[i]
+                str_set.add(s[i])
+
+            print(wordmap.items())
+
+        return True
+
+        # Time Complexity: O(n) where n is the length of pattern
+        # Space Complexity: O(n) where n is the length of patterns 
