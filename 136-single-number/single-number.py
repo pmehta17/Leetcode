3ea61cfd7641f37da7e43154c189470d927120b9
Pratham Mehta <81605115+pmehta17@users.noreply.github.com>
2024-08-27 01:57:43 -0400

    Time: 109 ms (63.41%) | Memory: 19 MB (54.49%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,6 +1,15 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+
+        # Bit manipulation
+        res = 0
+
+        for n in nums:
+            res = res ^ n
+        return res
         
+
+        ## Hash set solution
         seen = set()
 
         for n in nums:
