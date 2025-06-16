d172edc4a317656c5cbd6d5b6d7e6e22af53ddb9
Pratham Mehta <81605115+pmehta17@users.noreply.github.com>
2025-06-16 10:25:57 -0400

    Time: 14 ms (45.50%) | Memory: 17.8 MB (86.13%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,24 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
-        return sorted(s) == sorted(t)
+        # two anagrams will be the same when sorted
+        # time complexity: nlogn + mlogm
+        # return sorted(s) == sorted(t)
+
+        
 
         # two anagrams will have the same letter frequencies 
 
+        if len(s) != len(t): 
+            return False
+        
+        sCount, tCount = {}, {}          
+
+        for i in range(len(s)):
+            # update hashmap to add current char to count
+            sCount[s[i]] = 1 + sCount.get(s[i], 0)
+            tCount[t[i]] = 1 + tCount.get(t[i], 0)
+        return sCount == tCount
 
         
         
