252e37153e49cd0b7897712ede99a3c2daaecbff
Pratham Mehta <81605115+pmehta17@users.noreply.github.com>
2025-06-16 10:16:29 -0400

    Time: 19 ms (15.83%) | Memory: 18.5 MB (19.18%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,30 +1,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
+        return sorted(s) == sorted(t)
 
-        return collections.Counter(s) == collections.Counter(t)
-
-        # if len(s) != len(t):
-        #     return False
-
-        # s_map = {}
-        # t_map = {}
-
-        # for c in s:
-        #     if c in s_map:
-        #         s_map[c] += 1
-        #     else:
-        #         s_map[c] = 1
-
-        # for ch in t:
-        #     if ch in t_map:
-        #         t_map[ch] += 1
-        #     else:
-        #         t_map[ch] = 1
-
-        # return s_map == t_map	
+        # two anagrams will have the same letter frequencies 
 
 
-
-
-
+        
+        
