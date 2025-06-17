2ecaca62199fb0499f7931f47997f5335f4adc81
Pratham Mehta <81605115+pmehta17@users.noreply.github.com>
2025-06-17 15:18:22 -0400

    Time: 8 ms (52.73%) | Memory: 17.8 MB (99.39%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,34 +1,9 @@
-import re
-class Solution: 
-
-
+class Solution:
     def isPalindrome(self, s: str) -> bool:
-       
-        # METHOD 1 - CLEAN STRING AND COMPARE REVERSES
-        # clean_string = ""
-        # for a in s: 
-        #     if a.isalnum() or a.isdigit(): 
-        #         clean_string += a.lower()
-        # return a == a[::-1]
+        
+        s_clean = ""
+        for ch in s: 
+            if ch.isalnum():
+                s_clean += ch.lower()        
 
-        ## METHOD 2 - TWO POINTERS
-
-        l = 0
-        r = len(s)-1
-
-        while l < r:
-            while l < r and not s[l].isalnum():
-                l += 1
-            while l < r and not s[r].isalnum():
-                r -= 1
-
-            if s[l].lower() != s[r].lower():
-                return False
-
-            else:
-                l += 1
-                r -= 1
-
-        return True
-
-
+        return s_clean[::-1] == s_clean
