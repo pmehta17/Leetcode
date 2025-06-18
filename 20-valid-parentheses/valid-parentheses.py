1b92495f30b1047dff534f7d38a3e7377bacf53d
Pratham Mehta <81605115+pmehta17@users.noreply.github.com>
2025-06-17 22:46:27 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (10.22%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,36 +1,30 @@
 class Solution:
     def isValid(self, s: str) -> bool:
 
-        # Questions
-        # Are all characters in this string going to some type of bracket? We can assume there are no letters, numbers, etc?
-        # Are the three brackets in the example all types of brackets I can expect?
-        # How do nested brackets work here?
-
         if len(s) % 2 == 1:
-            return False
+            return False 
 
         stack = []
 
-        closeToOpen = {
-            ")" : "(", 
-            ']' : '[', 
-            '}' : "{"
+        bracket_mapping = {
+            ")" : "(",
+            "]" : "[",
+            "}" : "{"
         }
 
         for c in s: 
-            # Check if closing bracket
-            if c in closeToOpen:
-                if len(stack) > 0 and stack[-1] == closeToOpen[c]:
-                    stack.pop()
-                else:
-                    return False
+            # close bracket 
+            if c in bracket_mapping: 
+                if len(stack) < 1 or stack[-1] != bracket_mapping[c]:
+                    return False # unmatched bracket
+                stack.pop(-1)
 
-            # Open bracket
+            # open bracket 
             else:
                 stack.append(c)
+                
 
-        
+
         return len(stack) == 0
 
-        #Time Complexity: O(n) where n is length of string
-        # Space complexity: Worst case O(n) bc we could have to add every element to the stack (assuming there are no closing brackets)
+        
