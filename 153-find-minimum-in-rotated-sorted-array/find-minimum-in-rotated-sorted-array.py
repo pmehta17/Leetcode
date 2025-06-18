73effc650ffcca35705fdb4d7123736935cab301
Pratham Mehta <81605115+pmehta17@users.noreply.github.com>
2025-06-17 22:53:45 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (50.55%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,5 +1,15 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
+        # rotating the array means moving the last element to the beginning 
+        # rotating [1 2 3 4 5] twice would become [5 1 2 3 4] and then [4 5 1 2 3]
+        # goal: find min in log(n) time 
 
+        # l = 0 
+        # r = len(nums) - 1
+
+        # mid = l + (r-l)/2
         return min(nums)
+
+
+
         
