d571f4d6671eeff6a46d44dfdc4d304c560aa2bc
Pratham Mehta <81605115+pmehta17@users.noreply.github.com>
2025-07-16 04:30:28 -0400

    Time: 0 ms (100.00%) | Memory: 19.1 MB (51.66%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,26 +1,31 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        
+        ops = ("+", "-", "*", "/")
 
-        # Questions
-        # 1. Input validation: Are we only going to have numbers and the four valid operators?
-        # 2. What if we only have 2 numbers? or only 1 operator? (Edge case)
-        # 3. Divide by zero?
         stack = []
 
-        for val in tokens:
-
-            if val == "+":
-                stack.append((stack.pop()) + stack.pop())
-            elif val == "-":
-                a, b = stack.pop(), stack.pop()
-                stack.append(b - a)
-            elif val == "*":
-                stack.append(stack.pop() * stack.pop())
-
-            elif val == "/":
-                a, b = stack.pop(), stack.pop()
-                stack.append(int(b/a))
-            else:
+        for val in tokens: 
+            if val in ops: 
+                num_two = int(stack.pop())
+                num_one = int(stack.pop())
+                stack.append(self.solve(num_one, num_two, val))
+            else: 
                 stack.append(int(val))
 
+
         return stack[0]
+
+
+
+    def solve(self, num_one, num_two, op):
+        if op == "+":
+            return num_one + num_two
+        elif op == "-":
+            return num_one - num_two
+        elif op == "*":
+            return num_one * num_two
+        elif op == "/":
+            return int(num_one / num_two)
+        
+        
