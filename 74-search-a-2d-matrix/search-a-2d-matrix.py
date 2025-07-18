17b26f5a71dba458e391f454585e578b67b75174
Pratham Mehta <81605115+pmehta17@users.noreply.github.com>
2025-07-18 17:20:09 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (92.88%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,7 +1,59 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for row in matrix:
-            if target in row:
-                return True
 
+
+        m = len(matrix) # rows
+        n = len(matrix[0]) # cols
+    
+        l = 0
+        r = (m * n) - 1
+
+        while l <= r: 
+
+            mid = l + (r-l)//2
+
+            row = mid // n
+            col = mid % n
+
+            mid_val = matrix[row][col]
+
+            if mid_val > target:
+                r = mid - 1
+                
+            elif mid_val < target:
+                l = mid + 1
+
+            elif mid_val == target:
+                return True
         return False
+
+
+    # Brute Force
+        # return None # to avoid running code below this 
+        #     for row in matrix:
+        #         if target in row:
+        #             return True
+
+            # return False
+
+        ###### O(m logn) 
+        ## Find row, then binary search on row 
+
+        # for row in matrix: 
+
+        #     # correct row 
+        #     if target >= row[0] and target <= row[-1]: 
+        #         l = 0 
+        #         r = len(row) - 1
+
+        #         while l <= r:
+        #             mid = l + (r-l)//2
+        #             print(mid, row[mid])
+
+        #             if row[mid] > target:
+        #                 r = mid - 1
+        #             elif row[mid] < target:
+        #                 l = mid + 1
+        #             elif row[mid] == target:
+        #                 return True
+        # return False
