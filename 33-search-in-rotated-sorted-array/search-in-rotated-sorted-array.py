2a43203434737c6d8945028d1aa2ab73b4e3e643
Pratham Mehta <81605115+pmehta17@users.noreply.github.com>
2025-07-19 01:44:53 -0400

    Time: 0 ms (100.00%) | Memory: 18.3 MB (22.30%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,9 +1,8 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+        return -1
         
-
-
-        try:
-            return nums.index(target)
-        except ValueError:
-            return -1
