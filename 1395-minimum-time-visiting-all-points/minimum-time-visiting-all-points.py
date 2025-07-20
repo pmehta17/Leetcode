b6494cc5a475f259ad914edd83de69f963959329
Pratham Mehta <81605115+pmehta17@users.noreply.github.com>
2025-07-19 20:34:51 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (73.33%) - LeetSync

---
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        
+
+        ans = 0
+        x1, y1 = points.pop()
+
+        while points:
+            x2, y2 = points.pop()
+            ans += max(abs(x2 - x1), abs(y2 - y1))
+            x1, y1 = x2, y2
+
+        return ans
