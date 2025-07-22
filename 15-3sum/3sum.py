6f4e70465593f7d8c59b8dd4a86f29b8c9e41eed
Pratham Mehta <81605115+pmehta17@users.noreply.github.com>
2025-07-22 02:51:42 -0400

    Time: 597 ms (43.61%) | Memory: 20.8 MB (36.56%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,36 +1,38 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-
+        
+        ans = []
         nums.sort()
-        ans = set()
 
         for i in range(len(nums)):
+
+            # skip duplicates for first element
             if i > 0 and nums[i] == nums[i - 1]:
-                continue  # Skip duplicates for `i`
+                continue # skip to next iteration of i
 
-            j = i + 1
-            k = len(nums) - 1
+            l = i + 1
+            r = len(nums) - 1
 
-            while j < k:
-                cur_sum = nums[i] + nums[j] + nums[k]
+            while l < r: 
+                total = nums[i] + nums[l] + nums[r]
 
-                if cur_sum == 0:
-                    ans.add((nums[i], nums[j], nums[k]))
-                    j += 1
-                    k -= 1
 
-                    # Skip duplicates for `j` and `k`
-                    while j < k and nums[j] == nums[j - 1]:
-                        j += 1
-                    while j < k and nums[k] == nums[k + 1]:
-                        k -= 1
+                if total == 0: 
+                    ans.append([nums[i], nums[l], nums[r]])
 
-                elif cur_sum > 0:
-                    k -= 1
+                    #update pointers and skip duplicates
+                    l += 1
+                    r -= 1
+                    
+                    while l < r and nums[l] == nums[l - 1]: 
+                        l += 1
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
+                
+                elif total > 0: 
+                    r -= 1
                 else:
-                    j += 1
+                    l += 1
+        
+        return ans
 
-        return list(ans)  # Convert the set to a list before returning
-
-
-        ## Time complexity: O(n log n ) because we are sorting
