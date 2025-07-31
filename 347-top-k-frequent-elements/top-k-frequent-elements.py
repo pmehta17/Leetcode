f6675d54ef3388607e6c587c6127ad5de39f346a
Pratham Mehta <81605115+pmehta17@users.noreply.github.com>
2025-07-30 21:20:59 -0400

    Time: 5 ms (61.49%) | Memory: 21.3 MB (62.03%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,19 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        ans = [0] * k
+        ct = Counter(nums)
+
+        max_heap = []   
+
+        for num, freq in ct.items(): 
+            heapq.heappush(max_heap, [-1*freq, num])
         
+        for i in range(k):
+            ans[i] = heapq.heappop(max_heap)[1]
 
-        # 1. add numbers to hashamap
-        freq_map = Counter(nums)
-        print(freq_map)
-
-
-        heap = []
-        for num, freq in freq_map.items():
-            heapq.heappush(heap, (freq, num))
-            if len(heap) > k:
-                heapq.heappop(heap)
-        
-        ans = [] 
-        for _ in range(k):
-            ans.append(heapq.heappop(heap)[1])
         return ans
+       
+       
+
+            
