918cd58fb0ba6f355b13a0394810c75e6a0b8eff
Pratham Mehta <81605115+pmehta17@users.noreply.github.com>
2025-08-04 23:50:31 -0400

    Time: 2 ms (4.44%) | Memory: 18.1 MB (31.36%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -8,20 +8,21 @@ class Solution:
         while l < r: 
 
             mid = l + (r-l)//2
+            if nums[mid] < nums[r]:
+                r = mid 
+            else:
+                l = mid + 1
 
-            # found rotation
-            if nums[mid] > nums[mid + 1]:
-                return nums[mid+1]
-            elif nums[mid] < nums[mid + 1]:
-                r = mid - 1
+            print(nums[l:r+1])
+        return nums[l]
 
 
         #####################   BRUTE FORCE   ###########################################
         # brute force
 
-        # return min(nums)
-        m = float("inf")
-        for num in nums: 
-            m = min(num, m)
-        return m
+        # # return min(nums)
+        # m = float("inf")
+        # for num in nums: 
+        #     m = min(num, m)
+        # return m
 
